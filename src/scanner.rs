60fb808f496f042422af229c7373878ef3a169ae
Justin B. Lauricio <lauriciolau902@gmail.com>
2026-09-07 22:51:38 +0800

    fix: decrease implementation for out of scopes

--- src/scanner.rs
+++ src/scanner.rs
@@ -54,7 +54,7 @@ impl Scanner {
     // to be refractored
     fn scan_token(&mut self) {
         let c = self.advance();
-
+ 
         match c {
             // Grouping symbols
             '(' => self.add_token(TokenType::LeftParen, Literal::None),
@@ -63,94 +63,34 @@ impl Scanner {
             '}' => self.add_token(TokenType::RightBrace, Literal::None),
             '[' => self.add_token(TokenType::LeftBracket, Literal::None),
             ']' => self.add_token(TokenType::RightBracket, Literal::None),
-
+ 
             // Arithmetic operators
             '+' => self.add_token(TokenType::Plus, Literal::None),
             '-' => self.add_token(TokenType::Minus, Literal::None),
             '*' => self.add_token(TokenType::Star, Literal::None),
             '/' => self.add_token(TokenType::Slash, Literal::None),
             '%' => self.add_token(TokenType::Percent, Literal::None),
-
-            // Boolean operators
-            '<' => {
-                if self.match_char('=') {
-                    self.add_token(TokenType::LessEqual, Literal::None);
-                } else {
-                    self.add_token(TokenType::Less, Literal::None);
-                }
-            }
-            '=' => {
-                if self.match_char('=') {
-                    self.add_token(TokenType::EqualEqual, Literal::None);
-                } else {
-                    self.add_token(TokenType::Equal, Literal::None);
-                }
-            }
-            '!' => {
-                if self.match_char('=') {
-                    self.add_token(TokenType::BangEqual, Literal::None);
-                } else {
-                    self.add_token(TokenType::Bang, Literal::None);
-                }
-            }
-
+ 
+            // Boolean operators 
+            '<' => self.add_token(TokenType::Less, Literal::None),
+            '=' => self.add_token(TokenType::Equal, Literal::None),
+            '!' => self.add_token(TokenType::Bang, Literal::None),
+ 
             // Separators
             '.' => self.add_token(TokenType::Dot, Literal::None),
             ',' => self.add_token(TokenType::Comma, Literal::None),
-
-            // Literals
-            '"' => self.string(),
-            c if c.is_digit(10) => self.number(),
-            c if c.is_alphabetic() || c == '_' => self.identifier(),
-
+ 
             // Ignore whitespace
             ' ' | '\r' | '\t' => {}
             '\n' => self.line += 1,
-
+ 
             // Unrecognized character
             _ => {
-                // print an error message for unrecognized characters
                 eprintln!("Unexpected character: {}", c);
             }
-
         }
-
     }
 
-    fn identifier(&mut self) {
-        // Continue consuming characters while they are alphanumeric or underscores
-        while self.peek().is_alphanumeric() || self.peek() == '_' {
-            self.advance();
-        }
-
-        // Extract the lexeme from the source
-        let text: String = self.source[self.start..self.current].iter().collect();
-        let token_type = match text.as_str() {
-            "var" => TokenType::Var,
-            "if" => TokenType::If,
-            "else" => TokenType::Else,
-            "while" => TokenType::While,
-            "print" => TokenType::Print,
-            "true" => TokenType::True,
-            "false" => TokenType::False,
-            "nil" => TokenType::Nil,
-            _ => TokenType::Identifier,
-        };
-
-        self.add_token(token_type, Literal::None);
-    }
-
-    // checker if the next character matches the expected character
-    fn match_char(&mut self, expected: char) -> bool {
-        if self.is_at_end() {
-            return false;
-        }
-        if self.source[self.current] != expected {
-            return false;
-        }
-
-        self.current += 1;
-        true
-    }
+    // todo: advance(), add_token()
     
 }
