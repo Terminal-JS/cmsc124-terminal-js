c54e4bc843820babf939617b9c3944bb69a0580f
Justin B. Lauricio <lauriciolau902@gmail.com>
2026-09-15 07:58:53 +0800

    fix: handles first /n input

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,13 @@ fn run_prompt() {
             break;  // EOF
         }
 
+        if line.ends_with('\n') {
+            line.pop();
+            if line.ends_with('\r') {
+                line.pop();
+            }
+        }
+
         run(line);
     }
 }
